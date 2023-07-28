eae36faf05729eb70b5f56de8c46da7ef5b39301
hariniwinter <113656649+hariniwinter@users.noreply.github.com>
2023-07-28 14:46:26 +0530

    Add files via upload

---
+++ src/main/java/com/iamneo/security/config/SecurityConfiguration.java
@@ -0,0 +1,68 @@
+package com.iamneo.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+	
+	@Autowired
+	JwtAuthenticationFilter jwtAuthFilter;
+	@Autowired
+	AuthenticationProvider authenticationProvider;
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		http
+		.cors().configurationSource(corsConfigurationSource())
+		.and()
+		.csrf()
+		.disable()
+		.authorizeHttpRequests()
+		.requestMatchers("/api/v1/auth/**")
+		.permitAll()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		.and()
+		.authenticationProvider(authenticationProvider)
+		.addFilterBefore(jwtAuthFilter,UsernamePasswordAuthenticationFilter.class);
+		
+		return http.build();
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedOrigin("*"); // Allow all origins
+		configuration.addAllowedMethod("*"); // Allow all HTTP methods (GET, POST, etc.)
+		configuration.addAllowedHeader("*"); // Allow all headers (Authorization, Content-Type, etc.)
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		
+		return source;
+	}
+	
+	@Bean
+	public CorsFilter corsFilter() {
+		return new CorsFilter(corsConfigurationSource());
+	}
+}
